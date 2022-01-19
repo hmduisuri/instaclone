6abc9fcd95f2f65390a73577321241554ef47913
hmduisuri <hmduisuri@gmail.com>
2022-01-19 19:01:11 +0900

    deployement test 1

--- components/Stories.js
+++ components/Stories.js
@@ -14,9 +14,9 @@ function Stories() {
         }));
 
         setSuggesions(suggesions);
-
+console.log(suggesions)
     }, []);
-
+debugger;
     return (
         <div className = "flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
             {/* {session && (
--- components/StoriesDef.js
+++ components/StoriesDef.js
@@ -16,7 +16,7 @@ const {data: session} = useSession();
         console.log(stories);
     }, []);
 
-
+debugger
 
     return (
         <div className = "flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
