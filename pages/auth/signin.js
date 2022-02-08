9603b56c7641ac8e822bbd82b9f3f450d559976b
hmduisuri <hmduisuri@gmail.com>
2022-02-08 19:44:41 +0900

    remove serversideprops and add useEffect for signin

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,32 +1,46 @@
 import { async } from "@firebase/util"
 import {getProviders , signIn as signIntoProvider} from "next-auth/react";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 
-function signin(){
-  return(
-    <button>Signin with Google</button>
-  )
-}
+// function signin(){
+//   return(
+//     <button>Signin with Google</button>
+//   )
+// }
 //running on the browser
 // function signin({providers}) {
-//     return (
-//         //from te https://next-auth.js.org/v3/configuration/pages "auth sign in example"
-//         <>
-//         <Header/>
-//         <div className="flex flex-col items-center justify-center min-h-screen py-2 px-14 text-center">
-//            <img className="w-80" src="https://links.papareact.com/ocw" alt=""/>
-//         <div className="mt-40">
-//         {Object.values(providers).map((provider) => (
-//           <div key={provider.name}>
-//             <button className="p-3 bg-blue-400 border-2 rounded-lg text-white" onClick={() => signIntoProvider(provider.id, {callbackUrl:"https://myinstar.netlify.app/"})}>
-//               Sign in with {provider.name}
-//             </button>
-//           </div>
-//         ))}
-//         </div></div>
-//       </>
-//     )
-// }
+function signin() {
+  const [providers, setProviders] = useState([]);
+  
+  useEffect(() => {
+    const providers = async() =>{
+      const providerslist = await getProviders();
+      setProviders(providerslist);
+      debugger;
+    }
+    providers().catch(console.error);
+
+},[]);
+
+    return (
+        //from te https://next-auth.js.org/v3/configuration/pages "auth sign in example"
+        <>
+        <Header/>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 px-14 text-center">
+           <img className="w-80" src="https://links.papareact.com/ocw" alt=""/>
+        <div className="mt-40">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button className="p-3 bg-blue-400 border-2 rounded-lg text-white" onClick={() => signIntoProvider(provider.id, {callbackUrl:"https://myinstar.netlify.app/"})}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+        </div></div>
+      </>
+    )
+}
 
 //running on the middle server side render
 // export async function getServerSideProps(){
