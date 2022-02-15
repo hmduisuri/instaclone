01f49866ec79b4b8141e64a4b79e39d037224b59
hmduisuri <hmduisuri@gmail.com>
2022-02-15 20:20:40 +0900

    reinstall modules

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -24,7 +24,7 @@ export default NextAuth({
        //sub is the google userid commings back
        session.user.uid = token.sub;
 
-       return Promise.resolve(session).catch(function (err){console.log(err)});
+       return Promise.resolve(session).catch(err=>console.log(err));
     },
     // async jwt({ token, account }) {
     //   // Persist the OAuth access_token to the token right after signin
